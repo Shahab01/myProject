8b5449928dbcd6fa24bb023eb5ad95ffca6cde2a
Shahabuddin Ansari <ansari.shahabuddin999@gmail.com>
2018-11-04 21:41:38 +0530

    I have commited my projects

---
+++ src/main/java/usinggit/UploadFile1Application.java
@@ -0,0 +1,12 @@
+package usinggit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UploadFile1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UploadFile1Application.class, args);
+	}
+}
